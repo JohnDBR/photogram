49205e96e010f464601f0a79bb1864f3a7a7a9e9
John <djbarbosa@uninorte.edu.co>
2018-06-16 14:37:36 -0500

    the behaviour of the up button from the action bar was fixed

--- app/src/main/java/com/john/platzigram/activities/PictureDetailActivity.java
+++ app/src/main/java/com/john/platzigram/activities/PictureDetailActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
 import android.transition.Fade;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.john.platzigram.R;
@@ -17,8 +18,10 @@ import butterknife.ButterKnife;
 
 public class PictureDetailActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.collapsingToolbar) CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.collapsingToolbar)
+    CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +30,24 @@ public class PictureDetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         showToolbar("", true);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setEnterTransition(new Fade());
         }
     }
 
-    public void showToolbar(String tittle, boolean upButton){
+    public void showToolbar(String tittle, boolean upButton) {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(tittle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
         //collapsingToolbarLayout
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
