7e993dfd7afe28a3e3b80070295c21b1bb706105
John <djbarbosa@uninorte.edu.co>
2018-06-18 13:47:41 -0500

    problem with the new post activity was fixed. If an image was already choosed and the cellphone orientation changed the image changed too

--- app/src/main/java/com/john/platzigram/activities/NewPostActivity.java
+++ app/src/main/java/com/john/platzigram/activities/NewPostActivity.java
@@ -1,6 +1,7 @@
 package com.john.platzigram.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.john.platzigram.R;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
@@ -28,6 +30,8 @@ public class NewPostActivity extends AppCompatActivity {
     @BindView(R.id.ivPicture) ImageView ivPicture;
     @BindView(R.id.btnTakePicture) Button btnTakePicture;
     @BindString(R.string.toolbar_tittle_newPost) String toolbarTittle;
+
+    File imageSelected = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,18 +60,37 @@ public class NewPostActivity extends AppCompatActivity {
             @Override
             public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
                 //Handle the images
-//                Picasso.get().load(imageFile).into(ivPicture);
-                Bitmap image = BitmapFactory.decodeFile(imageFile.getPath());
-                Log.d("tag", imageFile.getPath());
+
+                imageSelected = imageFile;
+                setImage(imageSelected);
+
+//                Bitmap image = BitmapFactory.decodeFile(imageFile.getPath());
+//                Log.d("tag", imageFile.getPath());
+
 //                Bitmap img = Bitmap.createScaledBitmap(image, 400, 400, true);
 
-                ivPicture.setImageBitmap(image);
+//                ivPicture.setImageBitmap(image);
 
+//                android.support.media.ExifInterface exif = new android.support.media.ExifInterface(filePath);
+//                int rotation = exif.getAttributeInt(android.support.media.ExifInterface.TAG_ORIENTATION, android.support.media.ExifInterface.ORIENTATION_NORMAL);
             }
         });
     }
 
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (imageSelected != null){
+            setImage(imageSelected);
+        }
+    }
+
     public void pictureHandler(View view){
         EasyImage.openChooserWithGallery(this, "Selecciona una imagen", 0);
+    }
+
+    public void setImage(File imageFile){
+        Picasso.get().load(imageFile).into(ivPicture);
     }
 }
